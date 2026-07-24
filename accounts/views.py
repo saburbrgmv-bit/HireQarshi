0507cf71c292fa78122823cec47e77e7e1d46742
Sabur Ibragimov <sizning_emailingiz@gmail.com>
2026-07-24 07:10:53 +0500

    first commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,32 +1,31 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
-from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
+from django.urls import reverse, reverse_lazy
+from .forms import RegisterForm
+from django.views.generic import CreateView
 from django.contrib.auth import logout
-from django.urls import  reverse_lazy
 
 def home(request):
-  return render(request, 'accounts/home.html')
+    return render(request, 'accounts/home.html')
 
-class RegisterCreateView(CreateView):
-  form_class = RegisterForm
-  success_url = reverse_lazy('login')
-  template_name = 'accounts/register.html'
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = 'accounts/register.html'
+    success_url = reverse_lazy('login')
 
-class SignLoginView(LoginView):
-  template_name = 'accounts/login.html'
+class LoginUserView(LoginView):
+    template_name = 'accounts/login.html'
 
-def get_success_url(self):
-    user = self.request.user
-    if user.is_authenticated and hasattr(user, 'profile'):
+    def get_success_url(self):
+        user = self.request.user
         if user.profile.role == 'jobseeker':
-            return reverse_lazy('employer')
+            return reverse_lazy('employers')
         elif user.profile.role == 'employer':
-            return reverse_lazy('employer')
+            return reverse_lazy('employers')
+        return reverse_lazy('home')
 
-    return reverse_lazy('home')
 
 def exit(request):
-  logout(request)
-  return redirect('login')
+    logout(request)
+    return redirect('login')
